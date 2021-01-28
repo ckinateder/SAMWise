04c325a0eafbd5604eb49eb705c281e83896eba8
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-01-27 23:19:55 -0500

    fix issues with possible - speedup again

--- src/bouncer.py
+++ src/bouncer.py
@@ -166,7 +166,7 @@ class Bouncer:
                     actual_speedup = 0
                     inc = .001
                     ran = False
-                    while spread_w_fee > self.threshold+inc and actual_speedup < self.max_speedup-inc:
+                    while spread_w_fee > self.threshold+inc and actual_speedup < self.max_speedup:
                         buy_price = test_buy[1]['bid'] * \
                             (1+(actual_speedup/200))
                         sell_price = test_sell[1]['ask'] * \
