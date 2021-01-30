4fa6798a1938c131efeb7bee82b273bb3d952f16
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-01-29 19:55:46 -0500

    block xrp

--- src/main.py
+++ src/main.py
@@ -221,7 +221,8 @@ def getCommons(exchanges):
     out = list()
 
     for item in alls:
-        if alls.count(item) == len(exchanges) and 'USD' in item:  # Important - only allows usd
+        # Important - only allows usd
+        if alls.count(item) == len(exchanges) and 'USD' in item and not 'XRP' in item:
             out.append(item)
     out = list(set(out))
     return out
