3ad8b9f23886f99e716c02f483cca05528b4e62a
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-02-09 23:23:03 -0500

    update readme and pricer

--- src/pricer.py
+++ src/pricer.py
@@ -10,30 +10,106 @@ from bs4 import BeautifulSoup
 
 from hive import Hive
 
-alls = {}
-
+start = time.time()
 
 PROPS = {}
 
 
-async def getOneSymbol(exchange, symbol):
+def getOneSymbol(exchange, symbol, loud=False):
+    """
+    Get one single symbol. this may not be necessary
+    """
     s = time.time()
-    x = exchange.fetchTicker(symbol)
-    if exchange.id == "coinbasepro":
-        time.sleep(0.08)
-    print(f"got {symbol} symbols on {exchange} in {time.time()-s:.2f}s")
-    PROPS[symbol].append({exchange: x})
+    try_again = True
+    timeout = 2
+    count = 0
+    while try_again and count < timeout:
+        try:
+            resp = exchange.fetchTicker(symbol)
+            if exchange.id == "coinbasepro":
+                time.sleep(0.08)
+            if loud:
+                print(f"got {symbol} on {exchange} in {time.time()-s:.2f}s")
+            return resp
+        except ccxt.RateLimitExceeded:
+            print("RateLimitExceeded")
+            try_again = True
+            count += 1
+            time.sleep(5)
+    return {}
 
 
-async def divideSymbols(exchange, symbols):
-    tasks = []
-    for x in symbols:
-        tasks.append(asyncio.ensure_future(getOneSymbol(exchange, x)))
-    await asyncio.gather(*tasks, return_exceptions=True)
+def divideSymbols(exchange, symbols):
+    """
+    for exchanges that cant fetch all at once, divide up for each
+    """
+    intermediate = {}
+    for symbol in symbols:
+        resp = getOneSymbol(exchange, symbol)
+        # PROPS[symbol].append({exchange: resp})
+        if symbol in intermediate:
+            intermediate[symbol][exchange] = resp
+        else:
+            intermediate[symbol] = {exchange: resp}
+    return intermediate
+
+
+def mergeProps(one, two):
+    """
+    Merge one
+    {
+        'BTC/USD': {
+            ccxt.coinbasepro(): {...},
+            ccxt.kraken(): {...},
+            ccxt.binanceus(): {...},
+            ...etc
+        }
+    }
+    into two
+    {
+        'BTC/USD': {
+            ccxt.bitfinex(): {...},
+            ccxt.binance(): {...},
+            ccxt.phemex(): {...},
+            ...etc
+        }
+    }
+    """
+    inplace = two
+    final_out = {}
+    for symbol in one:
+        if symbol in two:
+            inplace[symbol].update(one[symbol])
+            final_out[symbol] = inplace[symbol]
+            inplace = two
+        else:
+            final_out[symbol] = one[symbol]
+    return final_out
+
+
+def myPrint(dic):
+    for x in dic:
+        print(f"'{x}': ", end="")
+        for exc in dic[x]:
+            print(f"{exc}: " + "{ ... }")
+
+
+def verify(test, sure):
+    """
+    Verify if test recieved as many responses as sure
+    """
+    verified = True
+    for sym in sure:
+        if sym in sure and sym in test:
+            if not set(sure[sym]) == set(test[sym].keys()):
+                verified = False
+        else:
+            verified = False
+    return verified
 
 
 # get slugs
-async def propagate():
+def propagate():
     """
     Get all tickers for each exchange, then propagate into dictionary. EACH CALL TO EACH EXCHANGE WILL BE ASYNC
     Ex:
@@ -51,21 +127,27 @@ async def propagate():
     idynmaics = h.transpose(dynamics)
     # set keys
     for key in list(dynamics.keys()):
-        PROPS[key] = []
+        PROPS[key] = {}
 
     # pprint(idynmaics)
     # fetch for ecah
-    for x in idynmaics:
-        if x.has["fetchTickers"]:
-            print(f"quick {x}")
-            y = x.fetchTickers(idynmaics[x])
-            for z in y:
-                PROPS[z].append({x: y[z]})
-        elif x.has["fetchTicker"]:
-            print(f"slow {x}")
-            await divideSymbols(x, idynmaics[x])
-    pprint(PROPS)
+    for exchange in idynmaics:
+        if exchange.has["fetchTickers"]:
+            print(f"quick {exchange}")
+            bulk = exchange.fetchTickers(idynmaics[exchange])
+            for symbol in bulk:
+                PROPS[symbol][exchange] = bulk[symbol]
+
+        elif exchange.has["fetchTicker"]:
+            print(f"slow {exchange} ...")
+            inter = divideSymbols(exchange, idynmaics[exchange])
+            # merge inter into PROPS
+            mergeProps(inter, PROPS)
+
+    # pprint(PROPS)
+    print(f"data makes sense: {verify(test=PROPS, sure=dynamics)}")
 
 
 if __name__ == "__main__":
-    asyncio.get_event_loop().run_until_complete(propagate())
+    propagate()
+    print(f"finished in {time.time()-start:.2f}s")
