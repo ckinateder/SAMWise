ae5d776dd7b7de2dae787d62f0567f66f74dd0d5
Calvin Kinateder <calvinkinateder1@yahoo.com>
2021-02-15 18:06:33 -0500

    fix minor error

--- src/propagator.py
+++ src/propagator.py
@@ -102,7 +102,7 @@ class Propagtor:
                 response = exchange.fetchTicker(ticker)
                 if exchange.id == "coinbasepro":
                     time.sleep(0.1)
-            except ccxt._rateLimitExceeded:
+            except ccxt.rateLimitExceeded:
                 tqdm.write(
                     colorBad(
                         f"Rate limit exceeded on {exchange} for {ticker} ... trying again in {waittime}"
